4c489698b05b6d4740d7c312b20b71fcb29df4ef
shusillimbu <shusillimbupangdhak@gmail.com>
2023-02-21 23:05:43 +0545

    updated: fourth commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,12 +7,12 @@ const Footer=(props)=>{
             <ul type='none' className='contacts'>
                 <li><strong>Phone no.: +977 9842903170</strong></li>
                 <li><strong>E-mail : shusillimbupangdhak@gmail.com</strong></li>
-                <li><a href='https://www.linkedin.com/in/shusil-pangdhak-45842b256/'><div className='linkedin'><img src="../assets/linkdein.png"/><strong>LinkedIn : Shusil Pangdhak</strong></div></a></li>
+                <li><a href='https://www.linkedin.com/in/shusil-pangdhak-45842b256/'><div className='linkedin'><img src="../linkdein.png"/><strong>LinkedIn : Shusil Pangdhak</strong></div></a></li>
             </ul>
             <ul type='none' className='social'>
-                <li><a href='https://www.facebook.com/shusillimbu.pangdhak'><div className="logo"><img src="./assets/facebook.webp"/><strong>Facebook</strong></div></a></li>
-                <li><a href='tel:9842903170'><div className="logo"><img src="./assets/WhatsApp.svg.webp"/><strong>+977 9842903170</strong></div></a></li>
-                <li><a href='#'><div className="logo"><img src="./assets/telegram.webp"/><strong>Telegram</strong></div></a></li>
+                <li><a href='https://www.facebook.com/shusillimbu.pangdhak'><div className="logo"><img src="./facebook.webp"/><strong>Facebook</strong></div></a></li>
+                <li><a href='tel:9842903170'><div className="logo"><img src="./WhatsApp.svg.webp"/><strong>+977 9842903170</strong></div></a></li>
+                <li><a href='#'><div className="logo"><img src="./telegram.webp"/><strong>Telegram</strong></div></a></li>
             </ul>
         </footer>
         </>
