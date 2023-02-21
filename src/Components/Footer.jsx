c0b8f2d999a3cfc8f83bd16050afdcc236de1519
shusillimbu <shusillimbupangdhak@gmail.com>
2023-02-21 23:18:50 +0545

    Fifth commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,12 +7,12 @@ const Footer=(props)=>{
             <ul type='none' className='contacts'>
                 <li><strong>Phone no.: +977 9842903170</strong></li>
                 <li><strong>E-mail : shusillimbupangdhak@gmail.com</strong></li>
-                <li><a href='https://www.linkedin.com/in/shusil-pangdhak-45842b256/'><div className='linkedin'><img src="../linkdein.png"/><strong>LinkedIn : Shusil Pangdhak</strong></div></a></li>
+                <li><a href='https://www.linkedin.com/in/shusil-pangdhak-45842b256/'><div className='linkedin'><img src="/images/linkdein.png"/><strong>LinkedIn : Shusil Pangdhak</strong></div></a></li>
             </ul>
             <ul type='none' className='social'>
-                <li><a href='https://www.facebook.com/shusillimbu.pangdhak'><div className="logo"><img src="./facebook.webp"/><strong>Facebook</strong></div></a></li>
-                <li><a href='tel:9842903170'><div className="logo"><img src="./WhatsApp.svg.webp"/><strong>+977 9842903170</strong></div></a></li>
-                <li><a href='#'><div className="logo"><img src="./telegram.webp"/><strong>Telegram</strong></div></a></li>
+                <li><a href='https://www.facebook.com/shusillimbu.pangdhak'><div className="logo"><img src="/images/facebook.webp"/><strong>Facebook</strong></div></a></li>
+                <li><a href='tel:9842903170'><div className="logo"><img src="/images/WhatsApp.svg.webp"/><strong>+977 9842903170</strong></div></a></li>
+                <li><a href='#'><div className="logo"><img src="/images/telegram.webp"/><strong>Telegram</strong></div></a></li>
             </ul>
         </footer>
         </>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -13,12 +13,12 @@ const Header=(props)=>{
         <>
         <header style={props.darkVariables}>
             <span id='count-visiter'>Visiters:{n-1}</span>
-        <a href="./a.png"><img src='./a.png' alt="Shusil Pangdhak"/></a>
+        <a href="./a.png"><img src='/images/a.png' alt="Shusil Pangdhak"/></a>
             <div>
             <p>Shusil Pangdhak</p>
             <span>React Js Developer</span>
             </div>
-            <a href='https://github.com/ShusilLimbu'><img className='github' src='./github.png'></img></a>
+            <a href='https://github.com/ShusilLimbu'><img className='github' src='/images/github.png'></img></a>
             <div className="form-check form-switch" id='darkmode' onClick={props.enableDark}>
             <input className="form-check-input bg-info" type="checkbox" role="switch" id="flexSwitchCheckChecked"/>
             <label className="form-check-label" htmlFor="flexSwitchCheckChecked"></label>
